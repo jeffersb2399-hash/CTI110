1779f98368fb065c241d8ee2cd4be06fd92984b5
jeffersb2399-hash <jeffersb2399@student.faytechcc.edu>
2026-04-04 13:24:12 -0400

    Add files via upload

---
+++ P1HW1_JeffersonBrandy.py
@@ -0,0 +1,33 @@
+# Brandy Jefferson
+# 4 April 2026
+# P1HW1
+# Caluculating exponents, addition, and subtraction
+
+# Calculating Exponents
+
+print("----------Exponents----------")
+print()
+
+base = int(input("Enter a base number:  "))
+exponent = int(input("Enter an exponent:  "))
+result = base ** exponent
+print(base, "raised to the power of", exponent, "is", result,"!!")
+print()
+
+# Calculating Addition and Subtraction
+
+print("----------Addition and Subtraction----------")
+print()
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+sum_result = num1 + num2
+final_result = sum_result - num3
+
+print(num1, "+", num2, "-", num3, "is equal to", final_result,"!!")
+print()
+
+
+
