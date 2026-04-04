e97c1b7ea7486502d156244e88b45f90b0cda031
jeffersb2399-hash <jeffersb2399@student.faytechcc.edu>
2026-04-04 19:56:48 -0400

    Add files via upload

--- P1HW2_JeffersonBrandy.py
+++ P1HW2_JeffersonBrandy.py
@@ -36,5 +36,24 @@ final_result = int(budget) - sum_result
 print("Remaining Budget:", final_result)
 print()
 
+#================================
+# Comment Block
+#================================
+# Display the message if the remaining budget is negative or positive
+# Ask the user to enter budget
+# Ask the user to enter travel destination
+# Ask the user to enter how much they think they will spend on gas
+# Ask the user to enter how much they think they will spend on accommodation/hotel
+# Ask the user to enter how much they need for food
+# Display the travel expenses
+# Display the destination and initial budget
+# Convert the fuel, accommodations, and food expenses to integers
+# Display the fuel, accommodations, and food expenses   
+# Calculate the sum of the fuel, accommodations, and food expenses
+# Calculate the remaining budget by subtracting the sum of expenses from the initial budget
+# Display the remaining budget
+
+
+
 
 
